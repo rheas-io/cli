026a4c0e1063cfd0d08364f262d1d299ed285747
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-05 20:18:36 +0530

    stable cli class

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,22 @@
-#!/usr/bin/env node
+import os from "os";
+import { NewProject } from "./newProject";
+import { ICommandHandler } from "./commandContract";
+import { KeyValue, ClassOf } from "@laress/contracts";
 
-import fs from "fs";
-import path from "path";
+export class Cli {
 
-export default class Cli {
+    /**
+     * Holds the array of handler classes by their command keyword.
+     * For eg, the new project creator handler will have a 'new' 
+     * keyword.
+     * 
+     * @var array
+     */
+    private _commands: KeyValue<ClassOf<ICommandHandler>> = {};
+
+    constructor() {
+        this.addCommand('new', NewProject);
+    }
 
     /**
      * Handles all the cli command requests. All the cli commands are
@@ -15,18 +28,75 @@ export default class Cli {
      * there are three arguments.
      */
     public handleRequest() {
-        // Check if there are three argument values.
-        if (process.argv.length < 3) {
-            console.log("No command provided, please provide a valid command");
-            this.printCommandLists();
-            return;
+        try {
+            // Check if there are three argument values.
+            if (process.argv.length < 3) {
+                throw new Error(this.messageWithCommands("No command provided, please provide a valid command"));
+            }
+            const command = process.argv[2];
+
+            if (!this.hasCommand(command)) {
+                throw new Error(this.messageWithCommands("Invalid command, please provide a valid command"));
+            }
+
+            // Creates an instance of the handler and send the request.
+            const commandHandler = new this._commands[command](this);
+            commandHandler.handle();
+        }
+        // All error will print a list of command list by default. This is on
+        // the assumption that the handler won't be throwing any errors.
+        catch (error) {
+            console.log(error.message || "Error processing command. Contact the development team.");
+            throw error;
         }
     }
 
     /**
+     * Adds a command to the command list.
      * 
+     * @param key 
+     * @param handlerClass 
      */
-    public printCommandLists(){
+    public addCommand(key: string, handlerClass: ClassOf<ICommandHandler>) {
+        this._commands[key] = handlerClass;
 
+        return this;
+    }
+
+    /**
+     * Checks if a command of the given name is registered or not.
+     * 
+     * @param key 
+     * @return boolean
+     */
+    public hasCommand(key: string): boolean {
+        return !!this._commands[key];
+    }
+
+    /**
+     * Returns an error response with commands list.
+     * 
+     * @param message 
+     * @return string
+     */
+    private messageWithCommands(message: string): string {
+        return message + os.EOL + this.getCommandsList();
+    }
+
+    /**
+     * Prints a list of application commands to the console. This lets user know,
+     * which all commands are available on laress cli.
+     * 
+     * @returns string
+     */
+    public getCommandsList(): string {
+        let commands = "";
+
+        Object.keys(this._commands).forEach(function (value, index) {
+            index = index + 1;
+            commands += `[${index}] ${value}` + os.EOL;
+        });
+
+        return commands;
     }
 }
