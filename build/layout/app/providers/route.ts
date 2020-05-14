b874b4e804c5f398250784450fafb8e67456ee89
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-15 01:58:23 +0530

    router cache the routes on boot

--- build/layout/app/providers/route.ts
+++ build/layout/app/providers/route.ts
@@ -1,5 +1,6 @@
 import { Router } from "../../routes/router";
 import { ServiceProvider } from "@rheas/core";
+import { IRouter } from "@rheas/contracts/routes";
 
 export class RouteProvider extends ServiceProvider {
 
@@ -8,6 +9,26 @@ export class RouteProvider extends ServiceProvider {
      * a singleton Router instance.
      */
     public register(): void {
-        this.app.singleton('router', () => new Router());
+        this.app.singleton('router', () => new Router(this.app));
+    }
+
+    /**
+     * Caches the routes on service boot.
+     * 
+     * Additional registrars, if any, has to be registered in this function.
+     * Calls the cacheRoutes to load all the endpoints into the router. Without
+     * caching the routes, the router is literally not usefull and all the registered
+     * routes will be simply hanging in the object.
+     */
+    public boot(): void {
+        const router = this.app.get<IRouter>('router');
+
+        if (router == null) return;
+
+        // Add additional route registrars here. By default, Rheas provides
+        // api and web registrars. All th routes will be loaded when the router
+        // caches the routes.
+
+        router.cacheRoutes();
     }
 }
