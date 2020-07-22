a0f2209e7c07f86333fb9c785b74e26f370a711b
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-07-22 23:19:45 +0530

    reads from env file

--- layout/configs/app.ts
+++ layout/configs/app.ts
@@ -1,32 +1,55 @@
+import { env } from "@rheas/support/helpers";
+
 export default {
     /**
      * The application name.
      */
-    name: 'Rheas Application',
+    name: env('APP_NAME', 'Rheas Application'),
+
+    /**
+     * The application production flag. By default rheas application
+     * configs are in debug mode. Change this to true in environment
+     * file before deploying to production.
+     */
+    production: env('PRODUCTION', false),
+
     /**
      * The port where http request has to be served. Rheas application
      * will listen to this port for http requests.
      */
-    port: 3000,
+    port: env('HTTP_PORT', 3000),
+
     /**
      * The port where secure requests are served. Application will listen to
      * this port for https requests.
      */
-    secure_port: 3001,
+    secure_port: env('HTTPS_PORT', 3001),
+
     /**
      * Prints log on console, when debug is set to true. Set this to false on
      * production as writing to console is a heavy operation.
      */
-    debug: true,
+    debug: env('APP_DEBUG', true),
+
     /**
      * Maintenance mode flag. When this flag is set true, the server will respond
      * back with a 503 json response/maintenance page html. No other request will pass
      * through.
      */
-    maintenance: false,
+    maintenance: env('MAINTENANCE', false),
+
     /**
      * Maintenance mode allowed ips. Requests from these ip's will bypass the maintenance
      * mode restrictions.
      */
-    allowed_ips: []
+    allowed_ips: [],
+
+    /**
+     * 
+     */
+    providers: {
+        'db': DatabaseProvider,
+        'router': RouteProvider,
+        'url': UrlServiceProvider,
+    }
 }
