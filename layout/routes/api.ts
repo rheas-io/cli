1e655d356d4cee1c4a44d850348cb54eac2b7612
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-07-22 23:17:28 +0530

    framework router architecture changed

--- layout/routes/api.ts
+++ layout/routes/api.ts
@@ -1,30 +1,7 @@
-import { RouteRegistrar } from "@rheas/routing";
 import { IRoute } from "@rheas/contracts/routes";
 
-export class ApiRoutes extends RouteRegistrar {
+const apiRoutes: IRoute[] = [
+    // All the api routes
+];
 
-    /**
-     * Register all the api specific middlewares in here.
-     * 
-     * @var array
-     */
-    protected _middlewares = [];
-
-    /**
-     * Route prefix.
-     * 
-     * @var string
-     */
-    protected _path = "api";
-
-    /**
-     * Register all the api routes inside the array.
-     * 
-     * @return array
-     */
-    public routesList(): IRoute[] {
-        return [
-
-        ];
-    }
-}
+export default apiRoutes;
